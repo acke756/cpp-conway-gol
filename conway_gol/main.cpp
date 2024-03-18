0f3cea3be343582d9d8c745b229ea99f1452786f
Axel Gällstedt <acke756@gmail.com>
2024-03-18 16:36:42 +0100

    Removed terminal UI code

--- conway_gol/main.cpp
+++ conway_gol/main.cpp
@@ -1,29 +1,8 @@
-#include <iostream>
 #include <string>
 
 #include <SDL2/SDL.h>
 
 #include <conway_gol/gol.hpp>
-
-void hide_cursor(std::ostream& out) {
-  out << "\033[?25l";
-}
-
-void show_cursor(std::ostream& out) {
-  out << "\033[?25h";
-}
-
-void clear_screen(std::ostream& out) {
-  out << "\033[;H\033[2J";
-}
-
-void enable_alt_buffer(std::ostream& out) {
-  out << "\033[?1049h";
-}
-
-void disable_alt_buffer(std::ostream& out) {
-  out << "\033[?1049l";
-}
 
 using namespace conway_gol;
 
@@ -202,31 +181,3 @@ int main(int argc, char** argv) {
   SDL_Quit();
   return retval;
 }
-
-/*
-int main(int argc, char** argv) {
-  conway_gol::Gol gol(80, 50);
-  auto& in = std::cin;
-  auto& out = std::cout;
-
-  enable_alt_buffer(out);
-  hide_cursor(out);
-  out.flush();
-
-  std::string line;
-  while (in.good()) {
-    clear_screen(out);
-    out << gol;
-    out.flush();
-
-    gol.update();
-    std::getline(in, line);
-  }
-
-  show_cursor(out);
-  disable_alt_buffer(out);
-  out.flush();
-
-  return EXIT_SUCCESS;
-}
-*/
