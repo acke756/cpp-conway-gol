3d5eec03bfa87bff4d09052ea9a57c92dfe3e537
Axel Gällstedt <acke756@gmail.com>
2024-03-21 10:22:19 +0100

    GolView now stores the rect to draw to

--- conway_gol/main.cpp
+++ conway_gol/main.cpp
@@ -28,11 +28,12 @@ class GolView {
   public:
     GolView() = delete;
 
-    GolView(SDL_Renderer* renderer, const Gol& gol):
+    GolView(SDL_Renderer* renderer, const Gol& gol, SDL_Rect draw_rect):
         pixel_format_(SDL_AllocFormat(SDL_PIXELFORMAT_RGB24)),
         texture_(SDL_CreateTexture(renderer, pixel_format_->format,
           SDL_TEXTUREACCESS_STREAMING, gol.width(), gol.height())),
-        gol_(gol) {
+        gol_(gol),
+        draw_rect_(draw_rect) {
     }
 
     GolView(const GolView&) = delete;
@@ -47,7 +48,7 @@ class GolView {
       return static_cast<bool>(texture_);
     }
 
-    int draw(SDL_Renderer* renderer, const SDL_Rect* dstrect) {
+    int draw(SDL_Renderer* renderer) {
       Uint8* pixels;
       int pitch;
 
@@ -70,7 +71,7 @@ class GolView {
 
       SDL_UnlockTexture(texture_.get());
 
-      if (SDL_RenderCopy(renderer, texture_.get(), NULL, dstrect) < 0) {
+      if (SDL_RenderCopy(renderer, texture_.get(), NULL, &draw_rect_) < 0) {
         SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't copy texture to renderer: %s", SDL_GetError());
         return EXIT_FAILURE;
       }
@@ -82,6 +83,7 @@ class GolView {
     unique_pixel_format_ptr pixel_format_;
     unique_texture_ptr texture_;
     const Gol& gol_;
+    SDL_Rect draw_rect_;
 };
 
 class GolController {
@@ -127,14 +129,20 @@ int on_create_renderer(SDL_Renderer* renderer) {
   conway_gol::Gol gol(80, 50);
   GolController controller(gol);
 
-  GolView gol_view(renderer, gol);
+  SDL_Rect gol_draw_rect{0};
+  if (SDL_GetRendererOutputSize(renderer, &gol_draw_rect.w, &gol_draw_rect.h) < 0) {
+    SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't get renderer size: %s", SDL_GetError());
+    return EXIT_FAILURE;
+  }
+
+  GolView gol_view(renderer, gol, gol_draw_rect);
   if (!gol_view) {
     SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create gol_view: %s", SDL_GetError());
     return EXIT_FAILURE;
   }
 
   for (;;) {
-    if (gol_view.draw(renderer, NULL) < 0) {
+    if (gol_view.draw(renderer) < 0) {
       SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't copy texture to renderer: %s", SDL_GetError());
       return EXIT_FAILURE;
     }
